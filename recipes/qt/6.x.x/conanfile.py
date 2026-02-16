26a0d1831c7796776f7e5137e58f44921171caf7
ericLemanissier <ericLemanissier@users.noreply.github.com>
2026-02-16 19:01:18 +0100

    Change Python_FIND_VIRTUALENV strategy to ONLY

--- recipes/qt/6.x.x/conanfile.py
+++ recipes/qt/6.x.x/conanfile.py
@@ -682,8 +682,9 @@ class QtConan(ConanFile):
             # Get CMake to find PipEnv's Python first to ensure html5lib is found
             tc.cache_variables['Python_FIND_UNVERSIONED_NAMES'] = 'FIRST'
             tc.cache_variables['Python_FIND_STRATEGY'] = 'LOCATION'
-            tc.cache_variables['Python_FIND_VIRTUALENV'] = 'FIRST'
+            tc.cache_variables['Python_FIND_VIRTUALENV'] = 'ONLY'
             tc.cache_variables['Python_FIND_REGISTRY'] = 'NEVER'
+            tc.cache_variables['Python_FIND_FRAMEWORK'] = 'NEVER'
         tc.generate()
 
     def package_id(self):
